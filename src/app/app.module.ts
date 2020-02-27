5cb31553269d89a086cf77051b8356057d84f0ca
nick-occ <nicholaso1980@gmail.com>
2020-02-26 23:23:50 -0500

    update references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { MapMenubarComponent } from './map/map-menubar/map-menubar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MapIdentifyComponent } from './map/map-identify/map-identify.component';
+import { MapIdentifyDialogComponent } from './map/map-identify/map-identify-dialog/map-identify-dialog.component';
+import { MatDialogModule, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    MapMenubarComponent,
+    MapIdentifyComponent,
+    MapIdentifyDialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    FormsModule,
+    MatInputModule
   ],
-  providers: [],
+  entryComponents: [
+    MapIdentifyDialogComponent
+  ],
+  providers: [
+    {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { appearance: 'fill'}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
