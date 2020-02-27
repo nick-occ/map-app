4e1d526fc6fb4d301bb194faa648f4c01f3b2be0
nick-occ <nicholaso1980@gmail.com>
2020-02-26 23:26:41 -0500

    mock of map object to feed to map service

---
+++ src/app/map/shared/mock-map.ts
@@ -0,0 +1,10 @@
+import { Map } from './map';
+
+// test data
+export const MAPS: Map = {
+    name: 'Campus Map',
+    url: 'https://openmaps.uncc.edu/opengis/rest/services/Construction/Construction/MapServer',
+    basemap: 'osm',
+    center: [-80.7366, 35.3081],
+    zoom: 17
+  };
