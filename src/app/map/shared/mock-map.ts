5ca2c2ec4a142286b9e6b87cda29238b038a9f81
nick-occ <nicholaso1980@gmail.com>
2020-03-05 23:04:22 -0500

    updating Map sample

--- src/app/map/shared/mock-map.ts
+++ src/app/map/shared/mock-map.ts
@@ -1,10 +1,12 @@
 import { Map } from './map';
 
 // test data
-export const MAPS: Map = {
-    name: 'Campus Map',
+export const MAPS: Map[] = [{
+    mapId: 1,
+    name: 'Construction Map',
     url: 'https://openmaps.uncc.edu/opengis/rest/services/Construction/Construction/MapServer',
-    basemap: 'osm',
-    center: [-80.7366, 35.3081],
-    zoom: 17
-  };
+  }, {
+    mapId: 2,
+    name: 'Basemap Map',
+    url: 'https://openmaps.uncc.edu/opengis/rest/services/Basemaps/NeoTerra/MapServer',
+  }];
