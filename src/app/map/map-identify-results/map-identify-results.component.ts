cfdaf953845d02f126af6895fadd444edcf657a6
nick-occ <nicholaso1980@gmail.com>
2020-05-13 12:05:27 -0400

    add graphic and zoom to feature on identify

--- src/app/map/map-identify-results/map-identify-results.component.ts
+++ src/app/map/map-identify-results/map-identify-results.component.ts
@@ -11,6 +11,7 @@ import { MapService } from '../shared/map.service';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatExpansionPanel} from '@angular/material/expansion';
+import {loadModules} from 'esri-loader';
 
 export interface IdData {
   field: string;
@@ -36,16 +37,84 @@ export class MapIdentifyResultsComponent implements OnInit, OnChanges {
   displayColumns: string[] = ['field', 'value'];
   dataSource = new MatTableDataSource();
   @ViewChild(MatSort, {static: true}) sort: MatSort;
+  graphic: {};
 
   constructor(private mapService: MapService) { }
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log('changes', changes);
-    this.layerName = this.currentResult.layerName;
-    this.attributes = this.currentResult.feature.attributes;
-    this.dataSource = new MatTableDataSource(this.mapService.formatIdAttributes(this.attributes));
-    this.dataSource.sort = this.sort;
+    if (this.currentResult) {
+      this.layerName = this.currentResult.layerName;
+      this.attributes = this.currentResult.feature.attributes;
+      this.dataSource = new MatTableDataSource(this.mapService.formatIdAttributes(this.attributes));
+      this.dataSource.sort = this.sort;
+      this.highlightArea(this.currentResult);
+    }
   }
+
+  // @ts-ignore
+  async highlightArea(result): void {
+
+    const [Graphic, SimpleMarkerSymbol, SimpleLineSymbol, SimpleFillSymbol] =
+      await loadModules([
+        'esri/Graphic',
+        'esri/symbols/SimpleMarkerSymbol',
+        'esri/symbols/SimpleLineSymbol',
+        'esri/symbols/SimpleFillSymbol']);
+
+    this.mapService.mapView.graphics.removeAll();
+
+    console.log(result.feature.geometry.type);
+    let symbol;
+    switch (result.feature.geometry.type) {
+      case 'polygon':
+        symbol = {
+          type: 'simple-fill',
+          color: [ 255, 0, 0, 0.5 ],
+          style: 'solid',
+          outline: {
+            color: 'white',
+            width: 1
+          }
+        };
+        break;
+      case 'polyline':
+        symbol =  {
+          type: 'simple-line',
+          color: 'orange',
+          width: '5px',
+          style: 'short-dot'
+        };
+        break;
+      case 'point':
+        symbol = {
+          type: 'simple-marker',
+          style: 'circle',
+          color: 'red',
+          size: '20px',
+          outline: {
+            color: [255, 255, 0, 0.5],
+            width: 3
+          }
+        };
+        break;
+    }
+
+    this.graphic = new Graphic({
+      symbol,
+      geometry: result.feature.geometry
+    });
+
+    this.mapService.mapView.graphics.add(this.graphic);
+
+
+  }
+
+
+  zoomToFeature() {
+    this.mapService.mapView.goTo(this.graphic);
+  }
+
 
   ngOnInit() {  }
 
