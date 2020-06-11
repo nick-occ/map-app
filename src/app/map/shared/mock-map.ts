f693d7001c598cce85b8bd6e6fd8da4592cf805c
nick-occ <nicholaso1980@gmail.com>
2020-06-10 23:56:33 -0400

    add mapType key

--- src/app/map/shared/mock-map.ts
+++ src/app/map/shared/mock-map.ts
@@ -2,11 +2,20 @@ import { Map } from './map';
 
 // test data
 export const MAPS: Map[] = [{
-    mapId: 1,
-    name: 'Construction Map',
-    url: 'https://openmaps.uncc.edu/opengis/rest/services/Construction/Construction/MapServer',
+  mapId: 1,
+  name: 'Construction Map',
+  url: 'https://openmaps.uncc.edu/opengis/rest/services/Construction/Construction/MapServer',
+  mapType: 'tileLayer'
   }, {
-    mapId: 2,
-    name: 'Basemap Map',
-    url: 'https://openmaps.uncc.edu/opengis/rest/services/Basemaps/NeoTerra/MapServer',
-  }];
+  mapId: 2,
+  name: 'Basemap Map',
+  url: 'https://openmaps.uncc.edu/opengis/rest/services/Basemaps/NeoTerra/MapServer',
+  mapType: 'tileLayer'
+  },
+  {
+    mapId: 3,
+    name: 'FY16-20 Map',
+    url: 'https://openmaps.uncc.edu/opengis/rest/services/Construction/FY16_FY20_Construction/MapServer',
+    mapType: 'dynamic'
+  }
+];
