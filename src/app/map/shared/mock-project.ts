0f84de1b7b4f2ee833192c3219351493ac928813
nick-occ <nicholaso1980@gmail.com>
2020-07-06 23:39:47 -0400

    add different projects

--- src/app/map/shared/mock-project.ts
+++ src/app/map/shared/mock-project.ts
@@ -1,11 +1,30 @@
 import { Project } from './project';
 
 // test data
-export const PROJECT: [Project] = [{
+export const PROJECT: Project[] = [{
     projectId: 1,
     name: 'Campus Map',
+    // TODO: allow adding basemap from map service
     basemap: 'osm',
     center: [-80.7366, 35.3081],
     zoom: 17,
-    maps: [2, 3]
+    maps: [4]
+  },
+  {
+    projectId: 2,
+    name: 'Construction Map',
+    // TODO: allow adding basemap from map service
+    basemap: 'osm',
+    center: [-80.7366, 35.3081],
+    zoom: 17,
+    maps: [3]
+  },
+  {
+    projectId: 3,
+    name: 'Campus Basemap',
+    // TODO: allow adding basemap from map service
+    basemap: 'osm',
+    center: [-80.7366, 35.3081],
+    zoom: 17,
+    maps: [2]
   }];
